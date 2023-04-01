d0cf6832ca47b093ce93e919e05d5cc59eb9ae77
Lajos Cseppentő <1053931+LajosCseppento@users.noreply.github.com>
2023-04-01 09:45:37 +0200

    Minor

--- ycc-keycloak-local/kc.sh
+++ ycc-keycloak-local/kc.sh
@@ -2,4 +2,4 @@
 SCRIPT_DIR="$( cd "$( dirname "$(readlink -f "${BASH_SOURCE[0]}")" )" && pwd)"
 source "$SCRIPT_DIR/config.sh"
 
-"keycloak-$KEYCLOAK_VERSION/bin/kc.sh" "$@"
+"$SCRIPT_DIR/keycloak-$KEYCLOAK_VERSION/bin/kc.sh" "$@"
