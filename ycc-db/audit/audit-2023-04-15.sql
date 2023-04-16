952bfaa9f41dc2fd2b0fea9575a2f3a62e3f8318
Lajos Cseppentő <1053931+LajosCseppento@users.noreply.github.com>
2023-04-16 10:56:02 +0200

    Naming is important

--- ycc-db/audit/audit-2023-04-15.sql
+++ ycc-db/audit/audit-2023-04-15.sql
@@ -6,10 +6,10 @@
 -- change.
 
 CREATE TABLE audit_log (
-    id                NUMBER GENERATED ALWAYS AS IDENTITY(START with 1 INCREMENT by 1) NOT NULL,
-    "DATE"            DATE DEFAULT SYSDATE NOT NULL,
-    application       VARCHAR2(200) NOT NULL,
-    "USER"            VARCHAR2(200) NOT NULL,
-    short_description VARCHAR2(200) NOT NULL,
-    long_description  CLOB
+    id          NUMBER GENERATED ALWAYS AS IDENTITY(START with 1 INCREMENT by 1) NOT NULL,
+    "DATE"      DATE DEFAULT SYSDATE NOT NULL,
+    application VARCHAR2(200) NOT NULL,
+    "USER"      VARCHAR2(200) NOT NULL,
+    description VARCHAR2(200) NOT NULL,
+    data        CLOB
 );
