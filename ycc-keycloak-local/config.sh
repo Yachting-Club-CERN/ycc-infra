dc1002401afccc71e4a3e3062e51bbc90233c260
Lajos Cseppentő <1053931+LajosCseppento@users.noreply.github.com>
2024-01-07 19:48:09 +0100

    Upgrade Keycloak to 22.0.4

--- ycc-keycloak-local/config.sh
+++ ycc-keycloak-local/config.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "$(readlink -f "${BASH_SOURCE[0]}")" )" && pwd)"
 
-KEYCLOAK_VERSION=21.0.1
+KEYCLOAK_VERSION=22.0.4
 # Use LOCAL for using local build output (useful for development), exact version to download from the package repository
-YCC_KEYCLOAK_PROVIDER_VERSION="0.4.0"
+YCC_KEYCLOAK_PROVIDER_VERSION="1.0.0"
 # Ideal for flat Git repo clone layout
 YCC_KEYCLOAK_PROVIDER_LOCAL_DIRECTORY="$SCRIPT_DIR/../../ycc-keycloak-provider/build/libs"
 
